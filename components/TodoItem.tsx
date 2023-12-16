1de25809f9499ea83ffd876006a5ddbbf59b00d5
Brian Reich <reichman213@gmail.com>
2023-12-16 11:41:07 -0700

    Clean up TodoItem

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -1,36 +1,49 @@
 import React from "react";
+import Button from "./Button";
 import Card from "./Card";
+import Checkbox from "./Checkbox";
 
 
 interface TodoItemProps extends React.PropsWithChildren {
-    onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
+    onDelete?: (e: React.MouseEvent<HTMLButtonElement>) => void;
     title?: string;
+    completed?: boolean;
+}
+
+interface TodoItemState {
+    completed: boolean;
 }
 
 
-export default class TodoItem extends React.Component<TodoItemProps> {
+export default class TodoItem extends React.Component<TodoItemProps, TodoItemState> {
     constructor(props: TodoItemProps) {
         super(props);
 
-        this.onClickHandler = this.onClickHandler.bind(this);
-        this.remove = this.remove.bind(this);
+        this.state = {
+            completed: this.props.completed || false
+        }
+
+        this.toggleComplete = this.toggleComplete.bind(this);
     }
 
-    onClickHandler(e: React.MouseEvent<HTMLDivElement>) {
-        if (this.props.onClick)
-            this.props.onClick(e);
-    }
 
-    remove() {
-        // TODO open delete modal
-
-        // TODO if okay, delete this element.
+    toggleComplete() {
+        this.setState({
+            completed: !this.state.completed
+        });
     }
     
     render() {
         return (
-            <Card title={ this.props.title || "" } onClick={ this.onClickHandler }>
-                { this.props.children }
+            <Card className="flex w-full items-center" title={ this.props.title || "" } >
+                {/* <input type="checkbox" className="mr-4" /> */}
+                <Checkbox onClick={ this.toggleComplete } className="mr-4" />
+                <div className={`inline-block w-full transition-all ease-linear ${(this.state.completed) ? "line-through" : "" }`} >
+                    <span className="line-slice"></span>
+                    { this.props.children }
+                </div>
+
+                <Button className="bg-red-500 text-white justify-end" onClick={ this.props.onDelete }>Delete</Button>
             </Card>
         );
     }
