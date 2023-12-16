a810df6adbd8444b774df700aafcdd3cc73295c8
Brian Reich <reichman213@gmail.com>
2023-12-16 11:41:59 -0700

    Create sign up/in pages

---
+++ pages/signup.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+const SignUp: NextPage = () => {
+    return (
+        <div>
+            <h1>Sign Up</h1>
+
+            
+        </div>
+    );
+};
+
+export default SignUp;
